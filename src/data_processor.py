5bcdf07b4b1c9630761947d206cffbe4307f7bd2
suvancim <albamariade2836@hotmail.com>
2025-12-28 19:24:31 -0300

    Add placeholder calculate_ice_velocity function
    This adds a placeholder function for ice velocity calculation as a starting point for implementing analysis of georeferenced rasters.

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,1 +1,7 @@
 # Data processing functions for Antarctic glacial analysis
+
+def calculate_ice_velocity(raster1, raster2):
+    """
+    Calculates ice velocity by comparing two georeferenced rasters taken at different times.
+    """
+    pass
